75255a70e25b2fc77690a5b3bc43716ce0e72829
Sverre H Huseby <shh@thathost.com>
2026-01-17 13:45:53 +0100

    chore: use curl instead of wget

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ go build
 
 if test ! -f regression-test.sh
 then
-    wget https://raw.githubusercontent.com/sverrehu/ccalc/refs/heads/main/regression-test.sh
+    curl -O https://raw.githubusercontent.com/sverrehu/ccalc/refs/heads/main/regression-test.sh
     chmod 755 regression-test.sh
 fi
 ./regression-test.sh
